86031087d13bc10d1bcbb75f10cfc0901608bf8a
Martin Krutzfeld <m.krutzfeld@gmail.com>
2018-03-09 22:55:33 +0100

    refactored datahandler

--- js/datahandler.js
+++ js/datahandler.js
@@ -1,24 +1,27 @@
 //Returns data from pflanzen.json
-function getData() {
+function getData(backendurl) {
 
     let resolvePromise = new Promise(
         function (resolve, reject) {
 
             let xhttp = new XMLHttpRequest(),
                 method = "GET",
-                url = "pflanzen.json";
+                url = backendurl + 'pflanzen.json';
             xhttp.open(method, url, true);
             xhttp.onreadystatechange = function () {
-                if (xhttp.readyState == 4 && xhttp.status == 200) {
-                    console.log(this.readyState, this.status);
+                if (xhttp.readyState == 4 ) {
+                  if (xhttp.status == 200){
                     let plants = JSON.parse(xhttp.responseText);
                     resolve(plants);
+                  } else {
+                    reject("Server answered with: " + xhttp.status);
+                  }
+
                 }
             }
             xhttp.send();
         }
     );
-    console.log('test getdata', resolvePromise);
     return resolvePromise;
 
 }
--- js/viewHandler.js
+++ js/viewHandler.js
@@ -1,55 +1,43 @@
-function drawData(plants, tableContainerId){
-  console.log('drawData', plants, tableContainer);
-  var tableContainer = document.getElementById(tableContainerId);
+function drawData(plants, tableContainer){
   var tableNode = document.createElement("table");
   tableContainer.appendChild(tableNode);
 
-
-  var tableHeaderTH = document.createElement("TH");
-  tableHeaderTH.appendChild(document.createTextNode("Family"));
-  tableNode.appendChild(tableHeaderTH);
-
-  tableHeaderTH = document.createElement("TH");
-  tableHeaderTH.appendChild(document.createTextNode("Watering"));
-  tableNode.appendChild(tableHeaderTH);
-
-  tableHeaderTH = document.createElement("TH");
-  tableHeaderTH.appendChild(document.createTextNode("Keeping"));
-  tableNode.appendChild(tableHeaderTH);
-
-  var currentPlant;
-  var currentPlantEntry;
-  var currentPlantTR;
-
-  var currentPlantFamilyTD;
-  var currentPlantWateringTD;
-  var currentPlantKeepingTD;
-
-  var currentTextNode;
+  initTable(tableNode);
 
   for (let index in plants){
-    console.log('Plant:', plants[index]);
-    currentPlant = plants[index];
-    currentPlantEntry = document.createElement('TR');
-    currentPlantTR = tableNode.appendChild(currentPlantEntry);
-
-    currentPlantFamilyTD = document.createElement('TD');
-    currentPlantKeepingTD = document.createElement('TD');
-    currentPlantWateringTD = document.createElement('TD');
-
-    currentTextNode = document.createTextNode(currentPlant.family);
-    currentPlantFamilyTD.appendChild(currentTextNode);
-
-    currentTextNode = document.createTextNode(currentPlant.wateringPerWeek);
-    currentPlantKeepingTD.appendChild(currentTextNode);
-
-    currentTextNode = document.createTextNode(currentPlant.keeping);
-    currentPlantWateringTD.appendChild(currentTextNode);
-
-    currentPlantTR.appendChild(currentPlantFamilyTD);
-    currentPlantTR.appendChild(currentPlantKeepingTD);
-    currentPlantTR.appendChild(currentPlantWateringTD);
-
+    addTRToTable(plants[index].family, plants[index].wateringPerWeek, plants[index].keeping, tableNode);
   }
+}
 
+function initTable(tableNode){
+  addTHToTable("Family", tableNode);
+  addTHToTable("Watering", tableNode);
+  addTHToTable("Keeping", tableNode);
+}
+
+function addTHToTable(headerText, tableNode ){
+
+  var tableHeaderTH = document.createElement("TH");
+  tableHeaderTH.appendChild(document.createTextNode(headerText));
+  tableNode.appendChild(tableHeaderTH);
+
+}
+
+function addTRToTable(FamilyText, WateringText, KeepingText, tableNode){
+
+  var TRNode = document.createElement('TR');
+  TRNode = tableNode.appendChild(TRNode);
+
+  addTDToTR(WateringText, TRNode);
+  addTDToTR(KeepingText, TRNode);
+  addTDToTR(FamilyText, TRNode);
+
+  tableNode.appendChild(TRNode);
+}
+
+function addTDToTR(TDText, TRNode){
+  var TDNode = document.createElement('TD');
+  var TextNode = document.createTextNode(TDText);
+  TDNode.appendChild(TextNode);
+  TRNode.appendChild(TDNode);
 }
