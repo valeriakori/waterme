b3f38505f4232d6dfd967b416d6392bf296965a1
valeria <a.valeria.koriatchenko@freenet.de>
2018-03-30 18:56:32 +0200

    Initial commit; Add htdocs/pflanze.php

---
+++ htdocs/pflanze.php
@@ -0,0 +1,29 @@
+<?php
+
+$plants;
+$url = "localhost:3306";
+$user = "root";
+$pw = "";
+$dbName = "test";
+
+$connect = new mysqli($url, $user, $pw, $dbName);
+
+$sql = "SELECT * FROM pflanze";
+
+$result = mysqli_query($connect, $sql);
+
+$json_array = array();
+while ($row = mysqli_fetch_assoc($result)) 
+{
+    $json_array[] = $row;
+}
+
+//JSON string wird nicht generiert
+echo json_encode($json_array);  
+
+// Das Folgende wird korrekt im Browser angezeigt
+echo "<pre>";
+print_r($json_array);
+echo "</pre>";
+
+?>
